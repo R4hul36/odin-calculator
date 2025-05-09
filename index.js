593bbff692af867a6a58e41b0befcec6639b167c
Rahul <rahul.wa47@gmail.com>
2025-05-09 21:33:49 +0530

    added functions for basic math operations

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 console.log('hello world!')
- 
+  
+
+const add = function (firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+const subtract = function (firstNum,secondNum) {
+    return firstNum - secondNum;
+}
+
+const multiply = function (firstNum,secondNum) {
+    return firstNum * secondNum;
+}
+
+const divide = function (firstNum,secondNum) {
+    return firstNum / secondNum;
+}
+
+const operate = function (firstNum, operator, secondNum) {
+    let result;
+    if(operator === "+") {
+        result = add(firstNum, secondNum);
+    }else if (operator === "-") {
+        result = subtract(firstNum, secondNum);
+    }else if (operator === "*") {
+        result = multiply(firstNum, secondNum);
+    }else {
+        result = divide(firstNum, secondNum);
+    }
+    return result;
+}
+
+console.log(operate(2, "*", 5));
