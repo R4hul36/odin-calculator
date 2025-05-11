f69e0aefd68cd486c7f4b0af261a335bfb9c7508
Rahul <rahul.wa47@gmail.com>
2025-05-11 21:42:34 +0530

    refactored the code to include more functions

--- index.js
+++ index.js
@@ -48,23 +48,13 @@ let operator = ''
 let secondNum = ''
 const buttons = document.querySelectorAll('.btn').forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    // console.log(typeof btn.textContent)
     const btnText = btn.textContent
     let result = operate(firstNum, operator, secondNum)
     if (btnText === "DE") {
-      if (secondNum !== '') {
-        secondNum = secondNum.slice(0, -1);
-      } else if (operator !== '') {
-        operator = '';
-      } else if (firstNum !== '') {
-        firstNum = firstNum.slice(0, -1);
-        console.log(firstNum);
-        
-      }
-      displayContent(`${firstNum} ${operator} ${secondNum}`);
-      return; 
+      deleteDisplayValue()
+      return;
     }
-
+    
     if(btnText === "AC") {
       firstNum = ''
       operator = ''
@@ -78,27 +68,20 @@ const buttons = document.querySelectorAll('.btn').forEach((btn) => {
         symbols.includes(btnText)) ||
       btnText === '='
     ) {
-      
       displayContent(result)
       firstNum = String(result)
       secondNum = ''
       operator = btnText === '=' ? '' : btnText
     } else if (numbers.includes(btnText) && !operator) {
-      if (firstNum.includes('.')) {
-        if (btnText === '.') {
-          return
-        }
+      if(!checkForDuplicateDecimals(firstNum, btnText)){
+        firstNum += btnText
       }
-      firstNum += btnText
     } else if (symbols.includes(btnText)) {
       operator = btnText
     } else if (firstNum !== '' && operator !== '') {
-      if (secondNum.includes('.')) {
-        if (btnText === '.') {
-          return
-        }
+      if(!checkForDuplicateDecimals(secondNum, btnText)){
+        secondNum += btnText
       }
-      secondNum += btnText
     }
     if(btnText !== "DE") {
       displayContent(`${firstNum} ${operator} ${secondNum}`)
@@ -107,6 +90,27 @@ const buttons = document.querySelectorAll('.btn').forEach((btn) => {
     console.log(firstNum, operator, secondNum)
   })
 })
+
+const checkForDuplicateDecimals = function (num, btnText) {
+  return num.includes(".") && btnText === "."
+
+}
+
+const deleteDisplayValue = function () {
+  
+    if (secondNum !== '') {
+      secondNum = secondNum.slice(0, -1);
+    } else if (operator !== '') {
+      operator = '';
+    } else if (firstNum !== '') {
+      firstNum = firstNum.slice(0, -1);
+      console.log(firstNum);
+      
+    }
+    
+    displayContent(`${firstNum} ${operator} ${secondNum}`);
+  
+  }
 
 
 
